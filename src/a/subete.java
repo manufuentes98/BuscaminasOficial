97e9b9d4db272bf23e1bbf2750bbb8ef786fb5c8
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-03-09 10:12:56 +0100

    EL MEMEVERSO 3
    SUBETE DIOS

--- src/a/subete.java
+++ src/a/subete.java
@@ -3,6 +3,6 @@ package a;
 public class subete {
 public static void main(String[] args) {
 	System.out.println("subete puto");
-	System.out.println("que te subas ya ostia");
+	System.out.println("que te subasya ostia");
 }
 }
